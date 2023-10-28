7175631abf68d02c2718af61708b86deda3d7968
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2023-10-28 16:08:38 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,56 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(a: number): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  comeIn(person: Person) {
+    if ((this.door = true)) {
+      this.tenants.push(person);
+    }
+  }
+  public abstract OpenDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super();
+      }
+  OpenDoor(key: Key) {
+    if (key === this.key) {
+      this.door = true;
+      console.log("door is open");
+    } else {
+      console.log("key not found");
+    }
+  }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
